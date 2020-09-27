758a116226d78f29457c2606a864c86726d216bb
kai422 <poi912713@gmail.com>
2020-09-27 14:53:39 +0800

    MV_rescale+I_P_B_indentifier

---
+++ cmake-build-debug/uninstall_target.cmake
@@ -0,0 +1,21 @@
+if(NOT EXISTS "/home/kai422/CLionProjects/openHEVC_v0.5(Mv_Rescale+sampling)/cmake-build-debug/install_manifest.txt")
+  message(FATAL_ERROR "Cannot find install manifest: /home/kai422/CLionProjects/openHEVC_v0.5(Mv_Rescale+sampling)/cmake-build-debug/install_manifest.txt")
+endif(NOT EXISTS "/home/kai422/CLionProjects/openHEVC_v0.5(Mv_Rescale+sampling)/cmake-build-debug/install_manifest.txt")
+
+file(READ "/home/kai422/CLionProjects/openHEVC_v0.5(Mv_Rescale+sampling)/cmake-build-debug/install_manifest.txt" files)
+string(REGEX REPLACE "\n" ";" files "${files}")
+foreach(file ${files})
+  message(STATUS "Uninstalling $ENV{DESTDIR}${file}")
+  if(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
+    exec_program(
+      "/home/kai422/.local/share/JetBrains/Toolbox/apps/CLion/ch-0/202.7319.72/bin/cmake/linux/bin/cmake" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+      OUTPUT_VARIABLE rm_out
+      RETURN_VALUE rm_retval
+      )
+    if(NOT "${rm_retval}" STREQUAL 0)
+      message(FATAL_ERROR "Problem when removing $ENV{DESTDIR}${file}")
+    endif(NOT "${rm_retval}" STREQUAL 0)
+  else(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
+    message(STATUS "File $ENV{DESTDIR}${file} does not exist.")
+  endif(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
+endforeach(file)
